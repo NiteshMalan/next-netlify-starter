217dd4b2f07d37ba0c0e834df965b23f92839d66
NiteshMalan <68332989+NiteshMalan@users.noreply.github.com>
2023-02-12 00:44:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,20 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+<form>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
       </main>
 
       <Footer />
