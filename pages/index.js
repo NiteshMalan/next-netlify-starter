764660c5953904fe747e48fca323ae108579d1dc
NiteshMalan <68332989+NiteshMalan@users.noreply.github.com>
2023-02-11 23:34:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,33 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to paraphrasecheck!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Now Detecting Paraphrase Plag is Easy </code>
+    
         </p>
+    
+    
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Your Role: <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </main>
 
       <Footer />
