edc6c04603fac7b07372a62913493d67ba7a1d0d
NiteshMalan <68332989+NiteshMalan@users.noreply.github.com>
2023-02-12 00:37:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to paraphrasecheck!" />
+        <Header title="Welcome to my app!" />
         <p className="description">
-          Now Detecting Paraphrase Plag is Easy </code>
-    
+          Get started by editing <code>pages/index.js</code>
         </p>
-    
-    
-
       </main>
 
       <Footer />
